a74694b2d493db6d64e2295f2d97cae7c60979ff
Simphiwe Mngadi <simphiwemngadi58@gmail.com>
2025-12-11 13:29:38 +0200

    Add files via upload

---
+++ SQL Server Query Code.sql
@@ -0,0 +1,60 @@
+-- Combining Order years
+with all_orders as (
+select
+OrderID,
+CustomerID,
+ProductID,
+OrderDate,
+Quantity,
+Revenue,
+COGS
+from Orders_2023
+
+union all
+
+select
+OrderID,
+CustomerID,
+ProductID,
+OrderDate,
+Quantity,
+Revenue,
+COGS
+from Orders_2024
+
+union all
+
+select
+OrderID,
+CustomerID,
+ProductID,
+OrderDate,
+Quantity,
+Revenue,
+COGS
+from Orders_2025)
+
+-- Main dataset query
+select 
+a.OrderID,
+a.CustomerID,
+c.Region,
+a.ProductID,
+a.OrderDate,
+dateadd(week,datediff(week,0,a.OrderDate),0) as WeekDate, --Creating Week Date variable
+c.CustomerJoinDate,
+a.Quantity,
+a.Revenue,
+case when a.Revenue is null then p.Price*a.Quantity else a.Revenue end as CleanedRevenue, -- Null Treatment
+a.COGS,
+a.Revenue-a.COGS as Profit,
+p.ProductName,
+p.ProductCategory,
+p.Price,
+p.Base_Cost
+from all_orders a
+left join customers c
+on a.CustomerID =c.CustomerID 
+left join products  p
+on a.ProductID = p.ProductID 
+where a.CustomerID is not null; -- dropping null values
